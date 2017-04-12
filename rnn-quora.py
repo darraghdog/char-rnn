c6df72a39ae7f8305c7718b1a1418a59cbe380c6
Ubuntu <ubuntu@ip-172-31-12-144.eu-west-1.compute.internal>
2017-04-12 13:31:45 +0000

    Oooo push it

--- rnn-quora.py
+++ rnn-quora.py
@@ -53,14 +53,14 @@ for word, i in tqdm(word_index.items()):
     if embedding_vector is not None:
         embedding_matrix[i] = embedding_vector
 
-# Create embeddint matrix using the word embeddings
+# Create embedding matrix using the word embeddings
 max_features = 200000
 filter_length = 5
 nb_filter = 64
 pool_length = 4
 
 model = Sequential()
-print('Build model...')
+print('Build Word embedding LTSM model...')
 
 model1 = Sequential()
 model1.add(Embedding(len(word_index) + 1,
@@ -82,8 +82,64 @@ model2.add(Embedding(len(word_index) + 1,
 model2.add(TimeDistributed(Dense(300, activation='relu')))
 model2.add(Lambda(lambda x: K.sum(x, axis=1), output_shape=(300,)))
 
+# Create a 1D convolution on the inputted embeddings
 
-print('Build model...')
+print('Build 1D convolutional model...')
+
+model3 = Sequential()
+model3.add(Embedding(len(word_index) + 1,
+                     300,
+                     weights=[embedding_matrix],
+                     input_length=40,
+                     trainable=False))
+model3.add(Convolution1D(nb_filter=nb_filter,
+                         filter_length=filter_length,
+                         border_mode='valid',
+                         activation='relu',
+                         subsample_length=1))
+model3.add(Dropout(0.2))
+
+model3.add(Convolution1D(nb_filter=nb_filter,
+                         filter_length=filter_length,
+                         border_mode='valid',
+                         activation='relu',
+                         subsample_length=1))
+
+model3.add(GlobalMaxPooling1D())
+model3.add(Dropout(0.2))
+
+model3.add(Dense(300))
+model3.add(Dropout(0.2))
+model3.add(BatchNormalization())
+
+model4 = Sequential()
+model4.add(Embedding(len(word_index) + 1,
+                     300,
+                     weights=[embedding_matrix],
+                     input_length=40,
+                     trainable=False))
+model4.add(Convolution1D(nb_filter=nb_filter,
+                         filter_length=filter_length,
+                         border_mode='valid',
+                         activation='relu',
+                         subsample_length=1))
+model4.add(Dropout(0.2))
+
+model4.add(Convolution1D(nb_filter=nb_filter,
+                         filter_length=filter_length,
+                         border_mode='valid',
+                         activation='relu',
+                         subsample_length=1))
+
+model4.add(GlobalMaxPooling1D())
+model4.add(Dropout(0.2))
+
+model4.add(Dense(300))
+model4.add(Dropout(0.2))
+model4.add(BatchNormalization())
+
+
+print('Build model embedding on raw words...')
 model5 = Sequential()
 model5.add(Embedding(len(word_index) + 1, 300, input_length=40, dropout=0.2))
 model5.add(LSTM(300, dropout_W=0.2, dropout_U=0.2))
@@ -129,4 +185,33 @@ merged_model.fit([x1, x2, x1, x2], y=y, batch_size=128, nb_epoch=200,
 # 323432/323432 [==============================] - 616s - loss: 0.2780 - acc: 0.8748 - val_loss: 0.4991 - val_acc: 0.7906ve
 # Epoch 7/200
 # 323328/323432 [============================>.] - ETA: 0s - loss: 0.2528 - acc: 0.8877Epoch 00006: val_acc improved from 0.79186 to 0.79443, saving model to data/q323432/323432 [==============================] - 676s - loss: 0.2528 - acc: 0.8877 - val_loss: 0.5045 - val_acc: 0.7944
+
+
+
+#########################################
+# Using Embeddings and raw text text
+#########################################
+# Epoch 4/200
+# 323432/323432 [==============================] - 619s - loss: 0.3228 - acc: 0.8543 - val_loss: 0.4253 - val_acc: 0.8060ve
+# Epoch 5/200
+# 323328/323432 [============================>.] - ETA: 0s - loss: 0.2833 - acc: 0.8743Epoch 00004: val_acc improved from 0.80691 to 0.80865, saving model to data/q323432/323432 [==============================] - 708s - loss: 0.2833 - acc: 0.8743 - val_loss: 0.4252 - val_acc: 0.8087
+# Epoch 6/200
+# 323328/323432 [============================>.] - ETA: 0s - loss: 0.2516 - acc: 0.8895Epoch 00005: val_acc improved from 0.80865 to 0.81155, saving model to data/q323432/323432 [==============================] - 711s - loss: 0.2516 - acc: 0.8895 - val_loss: 0.4387 - val_acc: 0.8115
+# Epoch 7/200
+# 323328/323432 [============================>.] - ETA: 0s - loss: 0.2270 - acc: 0.9016Epoch 00006: val_acc improved from 0.81155 to 0.81182, saving model to data/q323432/323432 [==============================] - 707s - loss: 0.2270 - acc: 0.9016 - val_loss: 0.4646 - val_acc: 0.8118
+# Epoch 8/200
+# 323328/323432 [============================>.] - ETA: 0s - loss: 0.2103 - acc: 0.9096Epoch 00007: val_acc improved from 0.81182 to 0.81685, saving model to data/q323432/323432 [==============================] - 705s - loss: 0.2103 - acc: 0.9096 - val_loss: 0.4690 - val_acc: 0.8169
+
+
+#########################################
+# Using Embeddings and 1D conv and raw text
+#########################################
+
+# Epoch 2/200
+# 323432/323432 [==============================] - 697s - loss: 0.4206 - acc: 0.8005 - val_loss: 0.4260 - val_acc: 0.7899 0.78363 to 0.78992, saving model to data/quora-weights.h5
+# Epoch 3/200
+# 323432/323432 [==============================] - 632s - loss: 0.3604 - acc: 0.8347 - val_loss: 0.4094 - val_acc: 0.8064 0.78992 to 0.80641, saving model to data/quora-weights.h5
+# Epoch 4/200
+# 323432/323432 [==============================] - 639s - loss: 0.3115 - acc: 0.8600 - val_loss: 0.4193 - val_acc: 0.8074 0.80641 to 0.80742, saving model to data/quora-weights.h5
+# Epoch 5/200
 
